3953515d9d9c050504e6690ed3c9590dc89813a5
Refúgio Lifestyle <luisenriquegomesportugal@hotmail.com>
2024-09-10 21:15:30 -0300

    try firebase webhooks

--- src/app/api/webhooks/pagamentos/pix/route.ts
+++ src/app/api/webhooks/pagamentos/pix/route.ts
@@ -3,20 +3,30 @@ import { PixReturn } from "@/types";
 import { ref, update, set, get } from "firebase/database";
 
 export async function POST(request: Request) {
-    const webhookResponse = new Response(request.body);
-    const data = await webhookResponse.json() as { pix: PixReturn[] }
+    try {
+        const webhookResponse = new Response(request.body);
+        const data = await webhookResponse.json() as { pix: PixReturn[] }
 
-    for (let pixReturn of data.pix) {
-        const refeventosPagamentos = ref(database, `eventosPagamentos/${pixReturn.txid}`)
-        const snapshotPagamento = await get(refeventosPagamentos)
+        for (let pixReturn of data.pix) {
+            const refeventosPagamentos = ref(database, `eventosPagamentos/${pixReturn.txid}`)
+            const snapshotPagamento = await get(refeventosPagamentos)
 
-        let [refPagamento] = Object.values<string>(snapshotPagamento.val())
-        await Promise.all([
-            set(ref(database, `${refPagamento}/status`), "CONCLUIDA"),
-            set(ref(database, `${refPagamento}/pagoEm`), pixReturn.horario),
-            set(ref(database, `${refPagamento}/pixID`), pixReturn.endToEndId)
-        ])
+            console.log("snapshotPagamento.val()", snapshotPagamento.val())
+
+            let [refPagamento] = Object.values<string>(snapshotPagamento.val())
+            console.log("refPagamento", refPagamento)
+
+            await Promise.all([
+                set(ref(database, `${refPagamento}/status`), "CONCLUIDA"),
+                set(ref(database, `${refPagamento}/pagoEm`), pixReturn.horario),
+                set(ref(database, `${refPagamento}/pixID`), pixReturn.endToEndId)
+            ])
+        }
+
+        return Response.json({})
     }
-
-    return Response.json({})
+    catch (e) {
+        console.error(e)
+        return Response.json({}, { status: 500 })
+    }
 }
