8e61a3c3f2f8844b90c3d257ebfb414201a1176c
Refúgio Lifestyle <luisenriquegomesportugal@hotmail.com>
2024-09-12 14:33:58 -0300

    pagoEm

--- src/app/api/webhooks/pagamentos/credit_card/[txid]/route.ts
+++ src/app/api/webhooks/pagamentos/credit_card/[txid]/route.ts
@@ -17,28 +17,23 @@ export async function POST(request: Request, { params }: Props) {
 
     const webhookResponse = new Response(request.body);
     const webhookResponseData = await webhookResponse.text()
-    console.log("webhookResponseData", webhookResponseData)
     const [_, notificationId] = webhookResponseData.split("=")
-    console.log("notificationId", notificationId)
 
     let { data: notifications } = await efipay
         .getNotification({ token: notificationId }) as { code: number, data: ChargeReturn[] }
 
-        console.log("getNotification", notifications)
     let notification = notifications
         .sort((a, b) => String(a.id).localeCompare(String(b.id)))
         .pop()
 
-        console.log("notification", notification)
     const refeventosPagamentos = ref(database, `eventosPagamentos/${params.txid}`)
     const snapshotPagamento = await get(refeventosPagamentos)
 
     let refPagamento = snapshotPagamento.val()
-    console.log("refPagamento", refPagamento)
     await Promise.all([
         set(ref(database, `${refPagamento}/status`), notification?.status.current),
-        set(ref(database, `${refPagamento}/pagoEm`), notification?.received_by_bank_at),
-        set(ref(database, `${refPagamento}/chargeID`), notification?.identifiers.charge_id)
+        set(ref(database, `${refPagamento}/chargeID`), notification?.identifiers.charge_id),
+        set(ref(database, `${refPagamento}/pagoEm`), notification?.received_by_bank_at || false)
     ])
 
     return Response.json({})
