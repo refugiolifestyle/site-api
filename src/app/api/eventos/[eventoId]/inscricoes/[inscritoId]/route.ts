a357111ef8e421d24aa4d209a6b7cee42f32deb1
Refúgio Lifestyle <luisenriquegomesportugal@hotmail.com>
2025-03-09 03:44:23 -0300

    validacao

--- src/app/api/eventos/[eventoId]/inscricoes/[inscritoId]/route.ts
+++ src/app/api/eventos/[eventoId]/inscricoes/[inscritoId]/route.ts
@@ -13,31 +13,31 @@ export async function GET(_: Request, { params }: ApiProps) {
     const snapshotInscrito = await get(refInscrito)
     let data = snapshotInscrito.val()
 
-    if (!snapshotInscrito.exists()) {
-        const refInscritoAnteriomente = ref(database, `membros/${params.inscritoId}`)
-        const snapshotInscritoAnteriomente = await get(refInscritoAnteriomente)
+    // if (!snapshotInscrito.exists()) {
+    //     const refInscritoAnteriomente = ref(database, `membros/${params.inscritoId}`)
+    //     const snapshotInscritoAnteriomente = await get(refInscritoAnteriomente)
 
-        if (!snapshotInscritoAnteriomente.exists()) {
-            return Response.json({
-                inscrito: {
-                    cpf: params.inscritoId,
-                    novo: true
-                }
-            })
-        }
+    //     if (!snapshotInscritoAnteriomente.exists()) {
+    //         return Response.json({
+    //             inscrito: {
+    //                 cpf: params.inscritoId,
+    //                 novo: true
+    //             }
+    //         })
+    //     }
 
-        const {visitante, rede, celula, telefone, cpf, ...membro} = snapshotInscritoAnteriomente.val()
-        data = {
-            ...membro,
-            visitante,
-            cpf: cpf.replaceAll(/[^\d]+/g, ''),
-            telefone: telefone.replaceAll(/[^\d]+/g, ''),
-            rede: visitante ? "" : rede,
-            celula: visitante ? "" : celula,
-            novo: true
-        }
+    //     const {visitante, rede, celula, telefone, cpf, ...membro} = snapshotInscritoAnteriomente.val()
+    //     data = {
+    //         ...membro,
+    //         visitante,
+    //         cpf: cpf.replaceAll(/[^\d]+/g, ''),
+    //         telefone: telefone.replaceAll(/[^\d]+/g, ''),
+    //         rede: visitante ? "" : rede,
+    //         celula: visitante ? "" : celula,
+    //         novo: true
+    //     }
 
-    }
+    // }
 
     let { eventos, ...inscrito } = data
     return Response.json({
