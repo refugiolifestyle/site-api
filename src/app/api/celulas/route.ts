600e3ac426a2bc7e1d3081108acc02b25e6e297c
Refúgio Lifestyle <luisenriquegomesportugal@hotmail.com>
2025-03-09 02:26:55 -0300

    dynamic celulas route

--- src/app/api/celulas/route.ts
+++ src/app/api/celulas/route.ts
@@ -2,6 +2,9 @@ import { database } from "@/configs/firebase";
 import { CelulaType } from "@/types";
 import { get, ref } from "firebase/database";
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 export async function GET() {
     const refCelulas = ref(database, "celulas")
     const snapshotCelulas = await get(refCelulas)
